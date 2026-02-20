907950a4fc7b5ce696de5095ed764f83f7cd5ffe
WahajAhmedKhanGP <164142549+WahajAhmedKhanGP@users.noreply.github.com>
2026-02-20 14:48:12 +0500

    20022026

--- Controllers/ResturantController.cs
+++ Controllers/ResturantController.cs
@@ -11,9 +11,9 @@ using System.Numerics;
 
 namespace RMS.Controllers
 {
-    //[Authorize]
-    //[Route("api/[controller]")]
-    //[ApiController]
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
     public class ResturantController : ControllerBase
     {
         private readonly RMSContext _context;
--- obj/Debug/net8.0/RMS.AssemblyInfo.cs
+++ obj/Debug/net8.0/RMS.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RMS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b9bff9836032f11a2021fcaea1dc0cc807f7336")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6653f0bf75e1e62dfb642c99e7606fb7379d4f70")]
 [assembly: System.Reflection.AssemblyProductAttribute("RMS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RMS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
